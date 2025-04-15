513a6ff2a833c2432f04c627a6ef4fbda3fe8364
Tobias Halomoan <agyasta1808@gmail.com>
2025-04-16 02:11:07 +0700

    fix : fixed phone number label

--- src/components/JoinCoolForm.tsx
+++ src/components/JoinCoolForm.tsx
@@ -393,7 +393,7 @@ const JoinCoolForm = () => {
               className={`block px-2.5 pb-2.5 pt-4 w-full text-base  bg-white rounded-lg border-1 ${
                 phoneNumber && !isValidIndonesianPhone(phoneNumber)
                   ? "border-red-500 text-red-500"
-                  : "border-gray-300 text-gray-900"
+                  : "border-gray-300"
               } appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
               placeholder=""
               autoFocus={false}
@@ -413,14 +413,14 @@ const JoinCoolForm = () => {
             />
             <Label
               htmlFor="floating_outlined_phone"
-              className={`absolute text-xs sm:text-sm  duration-300 transform -translate-y-[10px] sm:-translate-y-3 scale-75 top-2 z-10 origin-[0]  px-2 peer-focus:px-2 peer-focus:text-primary-light peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 peer-focus:background-transparent`}
+              className={`absolute text-xs text-gray-500 sm:text-sm duration-300 transform -translate-y-[10px] sm:-translate-y-3 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 peer-focus:background-transparent peer-focus:text-primary-light`}
             >
               <span
                 className={`${
                   phoneNumber && !isValidIndonesianPhone(phoneNumber)
                     ? "text-red-500"
-                    : "text-gray-500"
-                }`}
+                    : ""
+                } peer-focus:text-primary-light`}
               >
                 {phoneNumber && !isValidIndonesianPhone(phoneNumber)
                   ? "Invalid Phone Number!"
